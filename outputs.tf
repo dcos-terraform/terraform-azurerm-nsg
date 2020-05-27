fee7a46289821dd7d89d15a9ac50e717db8cc33e
vadymzagorodko <vadym.zagorodko@gmail.com>
2020-05-27 09:23:45 +0300

    Upgrade to terraform-0.12

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,61 @@
-output "bootstrap.nsg_name" {
+output "bootstrap_nsg_name" {
   description = "Network security group name of the bootstrap"
-  value       = "${element(concat(azurerm_network_security_group.bootstrap.*.name,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.bootstrap.*.name, [""]),
+    0,
+  )
 }
 
-output "bootstrap.nsg_id" {
+output "bootstrap_nsg_id" {
   description = "Network security group id of the bootstrap"
-  value       = "${element(concat(azurerm_network_security_group.bootstrap.*.id,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.bootstrap.*.id, [""]),
+    0,
+  )
 }
 
-output "masters.nsg_name" {
+output "masters_nsg_name" {
   description = "Network security group name of the masters"
-  value       = "${element(concat(azurerm_network_security_group.masters.*.name,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.masters.*.name, [""]),
+    0,
+  )
 }
 
-output "masters.nsg_id" {
+output "masters_nsg_id" {
   description = "Network security group id of the masters"
-  value       = "${element(concat(azurerm_network_security_group.masters.*.id,list("")),0)}"
+  value       = element(concat(azurerm_network_security_group.masters.*.id, [""]), 0)
 }
 
-output "private_agents.nsg_name" {
+output "private_agents_nsg_name" {
   description = "Network security group name of the private agents"
-  value       = "${element(concat(azurerm_network_security_group.private_agents.*.name,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.private_agents.*.name, [""]),
+    0,
+  )
 }
 
-output "private_agents.nsg_id" {
+output "private_agents_nsg_id" {
   description = "Network security group id of the private agents"
-  value       = "${element(concat(azurerm_network_security_group.private_agents.*.id,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.private_agents.*.id, [""]),
+    0,
+  )
 }
 
-output "public_agents.nsg_name" {
+output "public_agents_nsg_name" {
   description = "Network security group name of the public agents"
-  value       = "${element(concat(azurerm_network_security_group.public_agents.*.name,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.public_agents.*.name, [""]),
+    0,
+  )
 }
 
-output "public_agents.nsg_id" {
+output "public_agents_nsg_id" {
   description = "Network security group id of the public agents"
-  value       = "${element(concat(azurerm_network_security_group.public_agents.*.id,list("")),0)}"
+  value = element(
+    concat(azurerm_network_security_group.public_agents.*.id, [""]),
+    0,
+  )
 }
+
